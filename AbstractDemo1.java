816f8021db7b7814bd8e4e09f8f3fdf36f88ec20
Prajakta Jagtap <prajaktajagtap274@gmail.com>
2026-06-11 09:39:38 +0530

    Refactor abstract class and main method structure
    Updated class structure by renaming RMDDemo2 to AbstractDemo1 and corrected instantiation error.

--- AbstractDemo1.java
+++ AbstractDemo1.java
@@ -1,25 +1,18 @@
- abstract class Base
+abstract class Base
 {
-  public int i,j;
-  public int Addition(int a, int b)
-  {
-    return a+b;
-  }
-   abstract public int Substraction(int a, int b);
-
-
-
+    public int i,j;
+    public int Addition(int a, int b)
+    {
+        return a+b;
+    }
+    abstract public int Substraction(int a, int b);
 }
 
-
-
-class RMDDemo2
+class AbstractDemo1
 {
     public static void main(String A[])
     {
-    
-      Base bp = new Base();      
+        Base bp = new Base();   // Error
 
-       
     }
 }
