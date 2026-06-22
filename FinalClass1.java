59ae5d50ad6a8be415b37667addaa7e8d2ec72ff
Prajakta Jagtap <prajaktajagtap274@gmail.com>
2026-06-22 14:18:44 +0530

    Add FinalClass1 with Base and Derived classes

---
+++ FinalClass1.java
@@ -0,0 +1,27 @@
+final class Base
+{
+    public void fun()
+    {
+        System.out.println("Inside Base fun");
+    }
+}
+
+class Derived
+{
+    public Base bobj;       // Composition
+
+    public Derived()
+    {
+        bobj = new Base();
+    }
+}
+
+class FinalClass1
+{
+    public static void main(String A[])
+    {
+        Derived dobj = new Derived();
+
+        dobj.bobj.fun();
+    }
+}
