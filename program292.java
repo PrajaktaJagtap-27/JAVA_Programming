107f9ad891cfed62eb16d07c88aa0321581d9040
Prajakta Jagtap <prajaktajagtap274@gmail.com>
2026-07-07 23:18:06 +0530

    Add program292.java to print array elements

---
+++ program292.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class program292
+{
+    public static void main(String A[])
+    {
+        int Arr[] = {45,21,90,54,78};
+
+        for(int no : Arr)
+        {
+            System.out.println(no);
+        }
+    }
+}
